7868f72905654477272d234b777781d6fd067dc9
xodlf100 <xodlf100@gmail.com>
2024-11-10 23:05:24 +0900

    feat 원시타입 어노테이션

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,25 @@
+// number
+let num1: number = 123;
+
+// string
+let str1: string = "hello";
+let str2: string = 'hello';
+let str3: string = `hello`;
+let str4: string = `hello ${num1}`
+
+// boolean
+let bool1 : boolean = true;
+let bool2 : boolean = false;
+
+// null
+let null1 : null = null;
+
+// undefined
+let nude1 : undefined = undefined;
+
+// 리터럴 타입 : 값 그 자체가 타입이 됨
+let numA: 10 = 10;
+numA = 10 //10만 넣을 수 있음
+
+let strA: "hello" = "hello";
+strA = "hello" // 같은 hello만 가능
