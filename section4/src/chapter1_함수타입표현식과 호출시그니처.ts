9d799f5f98f2d2c6aaa6f4f46ecca6ed31be95f2
xodlf100 <xodlf100@gmail.com>
2024-11-18 23:04:21 +0900

    feat 함수 타입 표현식과 호출 시그니처

---
+++ section4/src/chapter1_함수타입표현식과 호출시그니처.ts
@@ -0,0 +1,26 @@
+/**
+ * 함수 타입 표현식 : function type expression
+ */
+
+type Operation = (a: number, b:number) => number;
+
+const add: Operation = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const multiply: Operation = (a, b) => a * b;
+const divide: Operation = (a, b) => a / b;
+
+/**
+ * 호출 시그니처
+ * (콜 시그니처)
+ */
+
+type Operation2 = {
+  (a: number, b: number): number;
+  name: string; // 하이브리드 타입
+}
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const multiply2: Operation2 = (a, b) => a * b;
+const divide2: Operation2 = (a, b) => a / b;
+
