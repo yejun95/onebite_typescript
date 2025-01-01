2814b08e3e9aaecc4577052c560e125319d789c8
xodlf100 <xodlf100@gmail.com>
2025-01-01 21:32:06 +0900

    feat chapter2 - Mapped 타입 기반의 유틸리티 - 2 * Pick, Omit, Record

---
+++ section10/src/chapter2.ts
@@ -0,0 +1,54 @@
+/**
+ * Pick<T, K>
+ * -> 뽑다, 고르다
+ * -> 객체 타입으로부터 특정 프로퍼티만 골라내는 타입
+ */
+
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+type Pick<T, K extends keyof T> = {
+  [key in K]: T[key];
+}
+
+const legacyPost: Pick<Post, "title" | "content"> = {
+  title: "옛날 글",
+  content: "옛날 컨텐츠",
+}
+
+/**
+ * Omit<T, K>
+ * -> 생략하다, 빼다
+ * -> 객체 타입으로 부터 특정 타입을 제거하는 타입
+ */
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+const noTItlePost: Omit<Post, "title"> = {
+  content: "",
+  tags: [],
+  thumbnailURL: "",
+}
+
+/**
+ * Record<K, V>
+ */
+
+type ThumbnailLegacy = {
+  large: {
+    url: string
+  },
+  medium: {
+    url: string
+  },
+  small: {
+    url: string
+  },
+}
+
+// ThumbnailLegacy와 동일한 코드 -> 인덱스 시그니처와 비슷하지만 조금은 강제적이다 
+type Thumbnail = Record<"large" | "medium" | "small", { url: string }>;
