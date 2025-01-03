27ca5ad19a2dff375dfdb426bfcdc1bfde336622
xodlf100 <xodlf100@gmail.com>
2025-01-04 01:36:08 +0900

    feat useState -> useReducer 변환

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,25 +1,59 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useReducer, useRef, useState } from 'react';
 import './App.css';
 import Editor from './components/Editor';
+import { Todo } from './types';
+import TodoItem from './components/TodoItem';
 
-interface Todo {
-  id: number;
-  content: string;
+type Action = {
+  type: "CREATE",
+  data: {
+    id: number;
+    content: string;
+  }
+} | { type: "DELETE"; id: number };
+
+function reducer (state: Todo[], action: Action) {
+  switch(action.type){
+    case 'CREATE': {
+      return [...state, action.data]
+    }
+    case 'DELETE': {
+      return state.filter((it) => it.id !== action.id);
+    }
+  }
 }
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
 
   const idRef = useRef(0);
 
   const onClickAdd = (text: string) => {
-    setTodos([
-      ...todos,
-      {
+    // setTodos([
+    //   ...todos,
+    //   {
+    //     id: idRef.current++,
+    //     content: text
+    //   }
+    // ]);
+
+    dispatch({
+      type: "CREATE",
+      data: {
         id: idRef.current++,
         content: text
       }
-    ]);
+    })
+  }
+
+  const onClickDelete = (id: number) => {
+    // setTodos(todos.filter((todo) => todo.id !== id));
+
+    dispatch({
+      type: "DELETE",
+      id: id,
+    })
   }
 
   useEffect(() => {
@@ -30,6 +64,11 @@ function App() {
     <div className="App">
       <h1>Todo</h1>
       <Editor onClickAdd={onClickAdd} />
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete}/>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ section11/src/components/TodoItem.tsx
@@ -0,0 +1,18 @@
+import { Todo } from "../types";
+
+interface Props extends Todo {
+  onClickDelete: (id: number) => void;
+}
+
+export default function TodoItem(props: Props) {
+  const onClickButton = () => {
+    props.onClickDelete(props.id);
+  }
+
+  return (
+    <div>
+      {props.id}번: {props.content}
+      <button onClick={onClickButton}>삭제</button>
+    </div>
+  )
+}
